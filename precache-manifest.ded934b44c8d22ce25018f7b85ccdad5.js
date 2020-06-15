9693194b4e08bdff4003cd0049cdf6a337a14944
Ivan <ivanzhao8@gmail.com>
2020-06-14 19:40:46 -0700

    Updates

---
+++ precache-manifest.ded934b44c8d22ce25018f7b85ccdad5.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9839ca64b6e29dc14b82e73a58fe745b",
+    "url": "/tarrey-town/index.html"
+  },
+  {
+    "revision": "754af398ed46377918cc",
+    "url": "/tarrey-town/static/css/2.67b36e1b.chunk.css"
+  },
+  {
+    "revision": "6fddbe17ae4c5df518dc",
+    "url": "/tarrey-town/static/css/main.9b295c0a.chunk.css"
+  },
+  {
+    "revision": "754af398ed46377918cc",
+    "url": "/tarrey-town/static/js/2.6d4d3fc9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tarrey-town/static/js/2.6d4d3fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fddbe17ae4c5df518dc",
+    "url": "/tarrey-town/static/js/main.ca2a3205.chunk.js"
+  },
+  {
+    "revision": "346fd814b32f0147a7b7",
+    "url": "/tarrey-town/static/js/runtime-main.e4ab05b8.js"
+  },
+  {
+    "revision": "384a2e8be42f4a1999f5ed8eef6180fe",
+    "url": "/tarrey-town/static/media/gerudo.384a2e8b.png"
+  },
+  {
+    "revision": "918445d590b6335e33aa6264becea782",
+    "url": "/tarrey-town/static/media/gerudo.918445d5.mp3"
+  },
+  {
+    "revision": "55691327f999e2ab471220bbd7d8f18a",
+    "url": "/tarrey-town/static/media/goron.55691327.mp3"
+  },
+  {
+    "revision": "69847eeed6a6636fd26d6a6467c7290e",
+    "url": "/tarrey-town/static/media/goron.69847eee.jpeg"
+  },
+  {
+    "revision": "7585e879edb5773e634c75bffb1439ab",
+    "url": "/tarrey-town/static/media/married.7585e879.mp3"
+  },
+  {
+    "revision": "a43107f242e6ee945472081d770cc60a",
+    "url": "/tarrey-town/static/media/married.a43107f2.jpg"
+  },
+  {
+    "revision": "70ff5eb7df233ce9f894bf5f82aabd62",
+    "url": "/tarrey-town/static/media/rito.70ff5eb7.mp3"
+  },
+  {
+    "revision": "f807f46a5ac0b66c78ea4be73b2fcefa",
+    "url": "/tarrey-town/static/media/rito.f807f46a.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/tarrey-town/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/tarrey-town/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/tarrey-town/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/tarrey-town/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b12547e127ec7ac67fbe96ea283acce8",
+    "url": "/tarrey-town/static/media/start.b12547e1.mp3"
+  },
+  {
+    "revision": "dee433f137f9d222e979512dd66b5ee4",
+    "url": "/tarrey-town/static/media/start_img.dee433f1.png"
+  },
+  {
+    "revision": "26f15eeb1e4a00473bc12757ebbce0a1",
+    "url": "/tarrey-town/static/media/trianglify-lowres.26f15eeb.png"
+  },
+  {
+    "revision": "b95cd485febad5edcb761e90920254f4",
+    "url": "/tarrey-town/static/media/zora.b95cd485.mp3"
+  },
+  {
+    "revision": "c6cae07538cc81729324b1e2814ab27e",
+    "url": "/tarrey-town/static/media/zora.c6cae075.jpg"
+  }
+]);
