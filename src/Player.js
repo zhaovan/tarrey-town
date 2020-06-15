495e416f217d1b4bd70b74b8e0fc81b7ecdb1a08
Ivan <ivanzhao8@gmail.com>
2020-06-14 22:46:29 -0700

    added an audio player at the bottom that allows users to scroll through the carousel if necessary

--- src/Player.js
+++ src/Player.js
@@ -10,6 +10,8 @@ import zoraAudio from "./assets/zora.mp3";
 import marriedAudio from "./assets/married.mp3";
 import town from "./assets/img/start_img.png";
 // import Stepper from "react-stepper-horizontal";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 import Slider from "react-slick";
 
@@ -31,36 +33,73 @@ const audioStyle = {
   margin: "auto",
 };
 
-const audioArray = document.getElementById("audio_clip");
+const imageStyle = { margin: "auto", width: "25%", borderRadius: "5%" };
+
+const audioArray = [
+  startAudio,
+  goronAudio,
+  gerudoAudio,
+  ritoAudio,
+  zoraAudio,
+  marriedAudio,
+];
+
+// const audioArray = document.getElementById("audio_clip");
 
 export class Player extends Component {
   constructor(props) {
     super(props);
     this.next = this.next.bind(this);
     this.previous = this.previous.bind(this);
+    this.nextSong = this.nextSong.bind(this);
+    this.prevSong = this.prevSong.bind(this);
     this.state = {
       currentSlide: 0,
       songTime: 0,
-      //   playing: [false, false, false, false, false, false],
+      currSong: startAudio,
     };
   }
 
   next() {
-    // const currentSongTime = audioArray[this.state.currentSlide].currentTime;
-    // const oldSlide = this.state.currentSlide
-    // const newSlide = this.state.currentSlide + 1;
-    // this.setState({ songTime: currentSongTime, currentSlide: newSlide, playing[oldSlide]: false, playing[newSlide]: true });
     this.slider.slickNext();
   }
   previous() {
     this.slider.slickPrev();
   }
+
+  nextSong() {
+    let song = this.state.currSong;
+    if (this.state.currentSlide < audioArray.length) {
+      song = audioArray[this.state.currentSlide + 1];
+    }
+    this.next();
+
+    this.setState({
+      ...this.state,
+      currSong: song,
+      currentSlide: this.state.currentSlide + 1,
+    });
+  }
+
+  prevSong() {
+    let song = this.state.currSong;
+    if (this.state.currentSlide > 0) {
+      song = audioArray[this.state.currentSlide - 1];
+    }
+    this.previous();
+    this.setState({
+      currSong: song,
+      currentSlide: this.state.currentSlide - 1,
+    });
+  }
+
   getCurrentTime() {}
 
   render() {
     const settings = {
       dots: true,
       infinite: false,
+      arrows: false,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -68,14 +107,20 @@ export class Player extends Component {
     return (
       <>
         <div style={{ display: "grid" }}>
-          <h2 style={{ margin: "auto", padding: "2% 25%" }}>Tarrey Town</h2>
+          <h2 style={{ margin: "auto", padding: "2% 25%", fontSize: "30px" }}>
+            Tarrey Town
+          </h2>
           <div
             style={{ margin: "auto", textAlign: "center", padding: "3% 25%" }}
           >
             Tarrey Town is a small town from the Legend of Zelda, Breath of the
             Wild. It's started from completing the side quest "From the Ground
             up" in which you build the town (from the ground up) by collecting
-            lots and lots of wood.
+            lots and lots of wood. The town grows and changes as you meet with
+            different members from different species and the music changes
+            dynamically as people are added. Whether it's the strong bassline of
+            the Gorons, or the high-pitched notes of the Rito people, this site
+            allows you to experience those changes.
           </div>
         </div>
 
@@ -85,106 +130,88 @@ export class Player extends Component {
               <div style={{ margin: "auto" }}>
                 The audio below plays the music of the town at the start
               </div>
-              <img
-                src={town}
-                alt=""
-                style={{ margin: "auto", width: "25%", borderRadius: "5%" }}
-              />
-              <audio
+              <img src={town} alt="" style={imageStyle} />
+              {/* <audio
                 controls
                 // autoplay="true"
                 src={startAudio}
                 style={audioStyle}
                 id="audio_clip"
-              ></audio>
+              ></audio> */}
             </div>
             {/* <button onClick={this.next}>Click to add on the Goron Sound</button> */}
           </div>
           <div>
             <div style={slideContainer}>
-              <img
-                src={goronImg}
-                alt=""
-                style={{ margin: "auto", width: "25%" }}
-              />
-              <audio
+              <img src={goronImg} alt="" style={imageStyle} />
+              {/* <audio
                 controls
                 src={goronAudio}
                 style={audioStyle}
                 id="audio_clip"
-              />
+              /> */}
             </div>
           </div>
           <div>
             <div style={slideContainer}>
-              <img
-                src={gerudoImg}
-                alt=""
-                style={{ margin: "auto", width: "25%" }}
-              />
-              <audio
-                controls
-                autoplay={true}
-                src={gerudoAudio}
-                style={audioStyle}
-                id="audio_clip"
-              ></audio>
+              <img src={gerudoImg} alt="" style={imageStyle} />
             </div>
           </div>
           <div>
             <div style={slideContainer}>
-              <img
-                src={ritoImg}
-                alt=""
-                style={{ margin: "auto", width: "25%" }}
-              />
-              <audio
+              <img src={ritoImg} alt="" style={imageStyle} />
+              {/* <audio
                 controls
                 src={ritoAudio}
                 style={audioStyle}
                 id="audio_clip"
-              ></audio>
+              ></audio> */}
             </div>
           </div>
           <div>
             <div style={slideContainer}>
-              <img
-                src={zoraImg}
-                alt=""
-                style={{ margin: "auto", width: "25%" }}
-              />
-              <audio
+              <img src={zoraImg} alt="" style={imageStyle} />
+              {/* <audio
                 controls
                 src={zoraAudio}
                 style={audioStyle}
                 id="audio_clip"
-              />
+              /> */}
             </div>
           </div>
           <div>
             <div style={slideContainer}>
-              <img
-                src={marriedImg}
-                alt=""
-                style={{ margin: "auto", width: "25%" }}
-              />
-              <audio
+              <img src={marriedImg} alt="" style={imageStyle} />
+              {/* <audio
                 controls
                 src={marriedAudio}
                 style={audioStyle}
                 id="audio_clip"
-              />
+              /> */}
             </div>
           </div>
         </Slider>
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
+        {/* <div style={{ textAlign: "center", marginTop: "50px" }}>
           <button className="button" onClick={this.previous}>
             Previous
           </button>
           <button className="button" onClick={this.next}>
             Next
           </button>
-        </div>
+        </div> */}
+        <AudioPlayer
+          src={this.state.currSong}
+          onPlay={(e) => console.log("onPlay")}
+          onClickNext={this.nextSong}
+          onClickPrevious={this.prevSong}
+          showSkipControls="true"
+          showJumpControls="false"
+          style={{
+            position: "absolute",
+            bottom: "0",
+            width: "100%",
+          }}
+        />
       </>
     );
   }
