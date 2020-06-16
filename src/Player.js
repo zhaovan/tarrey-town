612f3ff972da5782e34c0b5bfba901324e69b9a1
Ivan <ivanzhao8@gmail.com>
2020-06-15 22:15:00 -0700

    Added audio player and other visual effects

--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,4 @@
 import React, { Component } from "react";
-
-// import ReactPlayer from "react-player";
-// import "./App.css";
 import startAudio from "./assets/start.mp3";
 import goronAudio from "./assets/goron.mp3";
 import ritoAudio from "./assets/rito.mp3";
@@ -9,7 +6,7 @@ import gerudoAudio from "./assets/gerudo.mp3";
 import zoraAudio from "./assets/zora.mp3";
 import marriedAudio from "./assets/married.mp3";
 import town from "./assets/img/start_img.png";
-// import Stepper from "react-stepper-horizontal";
+import Stepper from "react-stepper-horizontal";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 
@@ -25,12 +22,15 @@ import marriedImg from "./assets/img/married.jpg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import piano from "./assets/icons/48/piano.png";
+import trombone from "./assets/icons/48/trombone.png";
+import dulcimer from "./assets/icons/dulcimer.png";
+import clarinet from "./assets/icons/clarinet.png";
+import guitar from "./assets/icons/48/electric-guitar.png";
+import bagpipe from "./assets/icons/bagpipe.png";
+
 const slideContainer = {
   display: "grid",
-};
-
-const audioStyle = {
-  margin: "auto",
 };
 
 const imageStyle = { margin: "auto", width: "25%", borderRadius: "5%" };
@@ -44,7 +44,7 @@ const audioArray = [
   marriedAudio,
 ];
 
-// const audioArray = document.getElementById("audio_clip");
+const instrumentArray = [piano, trombone, dulcimer, clarinet, guitar, bagpipe];
 
 export class Player extends Component {
   constructor(props) {
@@ -57,6 +57,7 @@ export class Player extends Component {
       currentSlide: 0,
       songTime: 0,
       currSong: startAudio,
+      currInstruments: [piano],
     };
   }
 
@@ -69,15 +70,19 @@ export class Player extends Component {
 
   nextSong() {
     let song = this.state.currSong;
-    if (this.state.currentSlide < audioArray.length) {
+    if (this.state.currentSlide < audioArray.length - 1) {
       song = audioArray[this.state.currentSlide + 1];
     }
-    this.next();
+    const newSlide = Math.min(
+      this.state.currentSlide + 1,
+      audioArray.length - 1
+    );
 
+    this.next();
     this.setState({
       ...this.state,
       currSong: song,
-      currentSlide: this.state.currentSlide + 1,
+      currentSlide: newSlide,
     });
   }
 
@@ -86,10 +91,12 @@ export class Player extends Component {
     if (this.state.currentSlide > 0) {
       song = audioArray[this.state.currentSlide - 1];
     }
+
+    const newSlide = Math.max(this.state.currentSlide - 1, 0);
     this.previous();
     this.setState({
       currSong: song,
-      currentSlide: this.state.currentSlide - 1,
+      currentSlide: newSlide,
     });
   }
 
@@ -97,9 +104,10 @@ export class Player extends Component {
 
   render() {
     const settings = {
-      dots: true,
+      dots: false,
       infinite: false,
       arrows: false,
+      draggable: false,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -108,48 +116,80 @@ export class Player extends Component {
       <>
         <div style={{ display: "grid" }}>
           <h2 style={{ margin: "auto", padding: "2% 25%", fontSize: "30px" }}>
-            Tarrey Town
+            Tarrey Town Quest
           </h2>
-          <div
-            style={{ margin: "auto", textAlign: "center", padding: "3% 25%" }}
-          >
-            Tarrey Town is a small town from the Legend of Zelda, Breath of the
-            Wild. It's started from completing the side quest "From the Ground
-            up" in which you build the town (from the ground up) by collecting
-            lots and lots of wood. The town grows and changes as you meet with
-            different members from different species and the music changes
-            dynamically as people are added. Whether it's the strong bassline of
-            the Gorons, or the high-pitched notes of the Rito people, this site
-            allows you to experience those changes.
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                margin: "auto",
+                //   textAlign: "center",
+                padding: "1% 1%",
+                backgroundColor: "lightgray",
+                borderRadius: "5%",
+                width: "35%",
+                fontFamily: "Arvo",
+              }}
+            >
+              Tarrey Town is a small town from the Legend of Zelda, Breath of
+              the Wild. It's started from completing the side quest "From the
+              Ground up" in which you build the town (from the ground up) by
+              collecting lots and lots of wood. The town grows and changes as
+              you meet with different members from different species and the
+              music changes dynamically as people are added. Whether it's the
+              strong bassline of the Gorons, or the high-pitched notes of the
+              Rito people, this site allows you to experience those changes. Use
+              the arrows on the audio player to switch between different
+              versions and hear the differences.
+            </div>
+            <div
+              style={{
+                margin: "auto",
+                width: "33%",
+                // backgroundColor: "lightblue",
+                // paddingRight: "50px",
+              }}
+            >
+              <h3>(Main) Instruments currently present:</h3>
+              <div>
+                {instrumentArray.map((val, index) => {
+                  if (index <= this.state.currentSlide) {
+                    return (
+                      <img
+                        src={val}
+                        alt={val.toString()}
+                        style={{
+                          width: "48px",
+                          color: "black",
+                          borderRadius: "5%",
+                          margin: "5px",
+                          //   backgroundColor: "white",
+                        }}
+                      />
+                    );
+                  }
+                })}
+              </div>
+            </div>
           </div>
         </div>
 
-        <Slider {...settings} ref={(c) => (this.slider = c)}>
+        <Slider
+          {...settings}
+          ref={(c) => (this.slider = c)}
+          style={{ paddingTop: "3%" }}
+        >
           <div>
             <div style={slideContainer}>
-              <div style={{ margin: "auto" }}>
+              {/* <div style={{ margin: "auto" }}>
                 The audio below plays the music of the town at the start
-              </div>
+              </div> */}
               <img src={town} alt="" style={imageStyle} />
-              {/* <audio
-                controls
-                // autoplay="true"
-                src={startAudio}
-                style={audioStyle}
-                id="audio_clip"
-              ></audio> */}
             </div>
             {/* <button onClick={this.next}>Click to add on the Goron Sound</button> */}
           </div>
           <div>
             <div style={slideContainer}>
               <img src={goronImg} alt="" style={imageStyle} />
-              {/* <audio
-                controls
-                src={goronAudio}
-                style={audioStyle}
-                id="audio_clip"
-              /> */}
             </div>
           </div>
           <div>
@@ -160,52 +200,42 @@ export class Player extends Component {
           <div>
             <div style={slideContainer}>
               <img src={ritoImg} alt="" style={imageStyle} />
-              {/* <audio
-                controls
-                src={ritoAudio}
-                style={audioStyle}
-                id="audio_clip"
-              ></audio> */}
             </div>
           </div>
           <div>
             <div style={slideContainer}>
               <img src={zoraImg} alt="" style={imageStyle} />
-              {/* <audio
-                controls
-                src={zoraAudio}
-                style={audioStyle}
-                id="audio_clip"
-              /> */}
             </div>
           </div>
           <div>
             <div style={slideContainer}>
               <img src={marriedImg} alt="" style={imageStyle} />
-              {/* <audio
-                controls
-                src={marriedAudio}
-                style={audioStyle}
-                id="audio_clip"
-              /> */}
             </div>
           </div>
         </Slider>
-        {/* <div style={{ textAlign: "center", marginTop: "50px" }}>
-          <button className="button" onClick={this.previous}>
-            Previous
-          </button>
-          <button className="button" onClick={this.next}>
-            Next
-          </button>
-        </div> */}
+
+        <div style={{ margin: "1%" }}>
+          <Stepper
+            steps={[
+              { title: "Original Town" },
+              { title: "With Goron" },
+              { title: "With Gerudo" },
+              { title: "With Rito" },
+              { title: "With Zora " },
+              { title: "Altogether now!" },
+            ]}
+            activeStep={this.state.currentSlide}
+          />
+        </div>
+        <div>{this.state.currSongTitle}</div>
         <AudioPlayer
           src={this.state.currSong}
           onPlay={(e) => console.log("onPlay")}
           onClickNext={this.nextSong}
           onClickPrevious={this.prevSong}
           showSkipControls="true"
-          showJumpControls="false"
+          showJumpControls={false}
+          loop="true"
           style={{
             position: "absolute",
             bottom: "0",
